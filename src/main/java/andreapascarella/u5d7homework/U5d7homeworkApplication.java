c915e88672fe71c9df1e5bd1cedc6f790b3d6451
pask517 <andreapascarella99@gmail.com>
2026-02-10 12:57:45 +0100

    added base

---
+++ src/main/java/andreapascarella/u5d7homework/U5d7homeworkApplication.java
@@ -0,0 +1,13 @@
+package andreapascarella.u5d7homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d7homeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d7homeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreapascarella/u5d7homework/U5d7homeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package andreapascarella.u5d7homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d7homeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
